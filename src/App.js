69e80d16739321ea4ccd67dbf28eb63cf3b8abb3
Aaron Garcia <aaronegarcia1@gmail.com>
2022-10-03 13:40:13 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Nav from "./page/Nav";
+import GlobalStyles from "./GlobalStyles";
+import Hero from "./page/Hero";
+import SubHero from "./page/SubHero";
+import About from "./page/About";
 
 function App() {
+  const [scrollLock, setScrollLock] = useState(true);
+  const [navOpen, setNavOpen] = useState(false);
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
+
+  const checkWidth = () => {
+    setPageWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    const loadTime = setTimeout(() => {
+      setScrollLock(false);
+    }, 3000);
+
+    return () => clearTimeout(loadTime);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", checkWidth);
+    return () => {
+      window.removeEventListener("resize", checkWidth);
+    };
+  }, [pageWidth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyles scrollLock={scrollLock} />
+      <Nav navOpen={navOpen} setNavOpen={setNavOpen} />
+      <Hero pageWidth={pageWidth} />
+      <SubHero />
+      <About />
     </div>
   );
 }
